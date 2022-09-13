2cbc20b21ad603d92922c1e011f3d46e0d62f239
Guilherme Rocha <guirocha12395@gmail.com>
2022-09-13 03:08:14 -0300

    Criando demais pastas

--- login/assets/js/script.js
+++ login/assets/js/script.js
@@ -1,26 +1,70 @@
-let createLoading = (obj) =>{
-    let load = {
-        creat : function(){
-            let card = document.createElement('div');
-            card.classList.add('bgLoad')
-            let load = `
-                            <div class="lds-roller">
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                            </div>
-                        `
-            card.innerHTML = load;
-            obj.appendChild(card);
-        },
-        remove : function(){
-            obj.querySelector('.bgLoad').remove()
+let Dadoslogin = sessionStorage.getItem('login')
+let showLogin = document.getElementById('showLogin')
+
+let ShowLogin = ()=>{
+    const login = document.querySelector('#login')
+    const cadastro = document.querySelector('#cadastro')
+    
+    login.querySelector('.link').querySelector('a').addEventListener('click',()=>{
+        login.classList.toggle('fadeOutLeft')
+        setTimeout(()=> {
+            cadastro.classList.remove('fadeOutLeft')
+            login.classList.toggle('hidden')
+            cadastro.classList.toggle('hidden')
+            cadastro.classList.add('fadeInLeft')
+        }, 100);
+        
+    })
+    
+    cadastro.querySelector('.link').querySelector('a').addEventListener('click',()=>{
+        cadastro.classList.toggle('fadeOutLeft')
+        setTimeout(()=> {
+            login.classList.remove('fadeOutLeft')
+            cadastro.classList.toggle('hidden')
+            login.classList.toggle('hidden')
+            login.classList.add('fadeInLeft')
+        }, 100);
+        
+    })
+}
+
+if(!!showLogin){
+    showLogin.addEventListener('click',()=>{ 
+    function ajax(url, method, callback, params = null) {
+        var obj;
+        try { 
+            obj = new XMLHttpRequest();  
+        }catch(e){   
+            try {     
+                obj = new ActiveXObject("Msxml2.XMLHTTP");     
+            } catch(e) {     
+                try { 
+                obj = new ActiveXObject("Microsoft.XMLHTTP");       
+                } catch(e) {       
+                alert("Your browser does not support Ajax.");       
+                return false;       
+                }     
+            }   
         }
+        obj.open(method, url, true);
+        obj.setRequestHeader("Content-Type", "text/html");
+        obj.onreadystatechange = function() {
+            if(obj.readyState == 4) {
+                callback(obj);
+            } 
+        }
+        obj.send(JSON.stringify(params));
+        return obj; 
     }
-    return load
+        var ajax = ajax('login/index.html', 'get',  function(obj) { 
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(obj.responseText, "text/html");
+            document.querySelector('body').classList.add('noScrool')
+            document.querySelector('body').appendChild(doc.querySelector('.boxLogin'))
+            ShowLogin()
+        })
+        console.log(Dadoslogin)
+    })
+}else{
+    ShowLogin()
 }
