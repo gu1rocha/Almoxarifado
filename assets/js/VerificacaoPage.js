471382e97f176a5814931fe23fdea252b2d553dc
Guilherme Rocha <guirocha12395@gmail.com>
2022-09-16 00:00:35 -0300

    Update VerificacaoPage.js

--- assets/js/VerificacaoPage.js
+++ assets/js/VerificacaoPage.js
@@ -5,8 +5,8 @@ pathname === 'index.html' || pathname === "" ? pathname = 'home' : ''
 if(!!Dadoslogin){
     let resources = ConsoltarBaseUsuarioResources(JSON.parse(Dadoslogin))
     if(!(resources[0].toString()).includes(pathname)){
-        window.location.href = '/'
+        window.location.href = '/Almoxarifado/'
     }
 }else{
-    window.location.href = '/'
+    window.location.href = '/Almoxarifado/'
 }
